8c31bec70976e33699b245d1903a0b19ac049b06
241057 <241057@std.hi-joho.ac.jp>
2025-05-16 12:02:48 +0900

    Mutsuki Okamoto R2A2-20 241057

--- src/Review1.java
+++ src/Review1.java
@@ -5,5 +5,6 @@ public class Review1 {
         System.out.print("今日は一日\n" + "洗濯日和です。");
         System.out.print("外に干しても大丈夫そうです。");
         System.out.print("文字は" + "\',文字列は\"で囲みましょう。");
+        //a
     }
 }
