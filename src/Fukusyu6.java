eae45375d75b5d4df9747625f22b74a530424d5f
241057 <241057@std.hi-joho.ac.jp>
2025-05-19 12:18:12 +0900

    Mutsuki Okamoto R2A2-20 241057

---
+++ src/Fukusyu6.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Fukusyu6 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        if(0 == num % 3 && 0 == num % 5){
+            System.out.println("FizzBuzz");
+        }else if(0 == num % 3){
+            System.out.println("Fizz");
+        }else if(0 == num % 5){
+            System.out.println("Buzz");
+        }
+    }
+}
