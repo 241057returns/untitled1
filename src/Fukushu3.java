1ffad3795f0e2153cde8295b8afc5caafd52d15c
241057 <241057@std.hi-joho.ac.jp>
2025-05-19 11:29:42 +0900

    Mutsuki Okamoto R2A2-20 241057

---
+++ src/Fukushu3.java
@@ -0,0 +1,8 @@
+public class Fukushu3 {
+    public static void main(String[] args) {
+        String str1 = "iphon";
+        char chr = 'X';
+        String output = str1 + chr;
+        System.out.println(output);
+    }
+}
