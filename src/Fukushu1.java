9ac46400a6589bfd456d87bebc8a999a8fe7a4e2
241057 <241057@std.hi-joho.ac.jp>
2025-05-19 11:28:40 +0900

    Mutsuki Okamoto R2A2-20 241057

---
+++ src/Fukushu1.java
@@ -0,0 +1,11 @@
+public class Fukushu1 {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        int sum = 0;
+        sum = num1 + num2;
+        System.out.println(sum);
+
+        //やっぱ模範解答はSystem.out.println(num1 + num2);だった。
+    }
+}
---
+++ src/Fukushu2.java
@@ -0,0 +1,9 @@
+public class Fukushu2 {
+    public static void main(String[] args) {
+        int num1 = 50;
+        double num2 = 20.0;
+        double mult = num1 * num2;
+        System.out.println(mult);
+        //模範はSystem.out.println(num1 * num2);だと推測
+    }
+}
