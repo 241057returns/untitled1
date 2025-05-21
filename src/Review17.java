0d3bac7eb47b5af42f7cff1badf2072c2acecd90
241057 <241057@std.hi-joho.ac.jp>
2025-05-21 10:24:57 +0900

    Mutsuki Okamoto R2A2-20 241057

--- src/Review17.java
+++ src/Review17.java
@@ -2,13 +2,15 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Review16 {
+public class Review17 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int num = Integer.parseInt(br.readLine());
         int sum = 0;
         for (int j = 1; j <= num; j++) {
-            sum += j;
+            if (j % 2 == 0) {
+                sum += j;
+            }
         }
         System.out.println(sum);
     }
