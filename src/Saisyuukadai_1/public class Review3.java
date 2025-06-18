f3b7c472035bc29ebe0ca85259bec22c7736c03e
241057returns <s2470258@s.do-johodai.ac.jp>
2025-06-18 10:18:28 +0900

    Delete src/Saisyuukadai_1/public class Review3.java

--- src/Saisyuukadai_1/public class Review3.java
+++
@@ -1,7 +0,0 @@
-public class Review3 {
-    public static void main(String[] args) {
-        System.out.println(1072 + (-50) + 5 * -10); // 3行目 
-        System.out.println(3 * 3 + 7 + "0" + 5 + 3); // 4行目 
-        System.out.println("7" + 3 * 2 + 5 + 6); // 5行目 
-    }
-}
