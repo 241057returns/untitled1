f8c6c1a4674adaf950188c0491ae01a8910c8886
241057returns <s2470258@s.do-johodai.ac.jp>
2025-06-18 10:18:11 +0900

    Delete src/Saisyuukadai_1/public class Review2.java

--- src/Saisyuukadai_1/public class Review2.java
+++
@@ -1,7 +0,0 @@
-public class Review2 {
-    public static void main(String[] args) {
-        System.out.println(20 + 30 + "50" + 7 + 1); 
-        System.out.println(90 + 5 + "5" + (10 - 10)); 
-        System.out.println(90 + 50);
-    }
-}
